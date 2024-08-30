bf83b8ee505f9c04ed891e635c8e70a6744bda4b
Mithun.U <116871732+Mithun-750@users.noreply.github.com>
2024-08-30 20:44:05 +0530

    handel error

--- components/HomeComponents/HeroSection.jsx
+++ components/HomeComponents/HeroSection.jsx
@@ -10,8 +10,9 @@ const HeroSection = () => {
                     Empowering Your Business with Easy Capital Solutions
                 </h1>
                 <p className="mt-6 text-gray-500">
-                    Get access to the financial support your business needs to grow. Whether you're an MSME or an aspiring entrepreneur, we're here to help.
+                    Get access to the financial support your business needs to grow. Whether you&apos;re an MSME or an aspiring entrepreneur, we&apos;re here to help.
                 </p>
+
                 <Link href={"/apply"}>
                     <button className="px-5 py-2 mt-6 text-sm font-medium leading-5 text-white capitalize bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none">
                         Apply for a Loan Today
@@ -22,8 +23,8 @@ const HeroSection = () => {
 
             <div className="flex justify-center mt-10">
                 <Image
-                    className="object-cover w-full h-96 rounded-xl lg:w-4/5" 
-                    src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80" 
+                    className="object-cover w-full h-96 rounded-xl lg:w-4/5"
+                    src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
                     alt="Business Growth"
                     width={1600}
                     height={400}
