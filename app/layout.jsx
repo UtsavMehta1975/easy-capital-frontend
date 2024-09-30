b37fe4f82e8d28245755e0573eb1160d2d4b608e
Mithun.U <116871732+Mithun-750@users.noreply.github.com>
2024-09-30 22:49:11 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,37 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
+
 export const metadata = {
   title: "EasyCapital",
   description: "Get your msme loans",
   icons: {
     icon: "/favicon.ico",
-  }
+  },
 };
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-4C7L5SRT85"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-4C7L5SRT85');
+          `}
+        </Script>
+      </head>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
